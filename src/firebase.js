d10c9087ab9432bac02024a7b1c842b19b45665c
unknown <manojprabakarrr5@gmail.com>
2021-04-22 19:56:55 +0100

    first

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAg0-_lX8kP6Id466HUHucS59qECZAMIeo",
+    authDomain: "facebookclone-426bb.firebaseapp.com",
+    projectId: "facebookclone-426bb",
+    storageBucket: "facebookclone-426bb.appspot.com",
+    messagingSenderId: "439998384914",
+    appId: "1:439998384914:web:958cb0ac1bcfc6f0e8c1cb",
+    measurementId: "G-1CP0YK9Y1K"
+  };
